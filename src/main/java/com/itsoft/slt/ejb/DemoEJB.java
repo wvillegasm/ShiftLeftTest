3a8a3675557df3c2b6c585ad30618112d82f682c
Wilfredo Villegas <wivisisorg@gmail.com>
2015-03-14 16:16:53 -0400

    First Test

---
+++ src/main/java/com/itsoft/slt/ejb/DemoEJB.java
@@ -0,0 +1,11 @@
+package com.itsoft.slt.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class DemoEJB {
+
+	public Integer sumInt(){
+		return 2+3;
+	}
+}
---
+++ src/test/java/com/itsoft/slt/ejb/DemoEJBTest.java
@@ -0,0 +1,37 @@
+package com.itsoft.slt.ejb;
+
+import static org.junit.Assert.*;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.itsoft.slt.ejb.DemoEJB;
+
+public class DemoEJBTest {
+
+	private static EJBContainer container;
+	private static Context context;
+	private static DemoEJB demoEjb;
+	
+	
+	@Before
+	public void init() throws Exception{
+		
+		container = EJBContainer.createEJBContainer();
+		context = container.getContext();
+		demoEjb = (DemoEJB) context.lookup("java:global/classes/DemoEJB!com.itsoft.slt.ejb.DemoEJB");
+		System.out.println("Test ends");
+		
+	}
+	
+	@Test
+	public void sum_two_number_demoejb() {
+		Integer result = demoEjb.sumInt();
+		Integer expected = 5;
+		assertTrue(result.equals(expected));
+	}
+
+}
